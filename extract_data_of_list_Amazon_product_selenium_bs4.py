f526aa07f86d1dbc897af7b46e4ddee3541b8817
Duc Trung <trung.lyduc18@gmail.com>
2024-08-28 13:28:45 +0700

    COMPLETE: extract data of a list Amazon product (selenium, bs4)

--- extract_data_of_list_Amazon_product_selenium_bs4.py
+++ extract_data_of_list_Amazon_product_selenium_bs4.py
@@ -72,7 +72,11 @@ class Extactor:
 
         self.driver.execute_cdp_cmd(
             "Network.setUserAgentOverride",
-            {"userAgent": self.user_agents[random.randint(0, len(self.user_agents))]},
+            {
+                "userAgent": self.user_agents[
+                    random.randint(0, len(self.user_agents) - 1)
+                ]
+            },
         )
 
         self.driver.execute_script("return navigator.userAgent;")
@@ -133,8 +137,6 @@ class Extactor:
 
             self.driver.get(url_list[index])
 
-            time.sleep(3.2)
-
             # Choose location according zipcode
             if index == 0:
                 self.choose_location_to_delivery_to()
@@ -142,6 +144,8 @@ class Extactor:
             self.driver.refresh()
 
             self.soup = BeautifulSoup(self.driver.page_source, "html.parser")
+
+            print(f"\nPRODUCT #{index + 1} " + "=" * 10)
 
             # CATEGORY_ASIN (get from parameter)
 
@@ -154,149 +158,86 @@ class Extactor:
             title = self.soup_try_to_find("span", {"id": "productTitle"})
 
             # REVIEWS
-            reviews = self.soup.find(
-                "span", {"id": "acrCustomerReviewText"}
-            ).text.strip()
-            if reviews:
-                reviews = int(re.match(r"\d+", reviews).group())
+            review_num = self.soup_try_to_find("span", {"id": "acrCustomerReviewText"})
+            if review_num:
+                review_num = int(re.match(r"\d+", review_num).group())
 
-            print(f"ID: {id}")
+            # RATING
+            rating = self.soup_try_to_find(
+                "span",
+                {"data-hook": "rating-out-of-text"},
+            )
+            if rating:
+                rating = float(re.search(r"\d+(\.\d+)?", rating).group())
+
+            # DESCRIPTION
+            ul_element = self.soup.find(
+                "ul", class_="a-unordered-list a-vertical a-spacing-mini"
+            )
+
+            # Find all <li> elements within the <ul>
+            li_elements = ul_element.find_all("li", class_="a-spacing-mini")
+            description = []
+            for li in li_elements:
+                description.append(li.text.strip())
+
+            # COLOR
+            color = self.soup_try_to_find("span", {"class": "selection"})
+
+            # PRICE
+            price = float(
+                re.findall(
+                    r"\d+\.\d+|\d+",
+                    self.soup_try_to_find("span", {"class": "aok-offscreen"}),
+                )[0]
+            )
+
+            # STATUS
+            status = self.soup_try_to_find(
+                "span", {"class": "a-size-medium a-color-success"}
+            )
+
+            # LAST MONTH SOLD
+            last_month_sold = self.soup_try_to_find(
+                "span", {"id": "social-proofing-faceout-title-tk_bought"}
+            )
+            match = re.search(r"\d+(?:[KkMm])?", last_month_sold)
+            if match:
+                last_month_sold = match.group()
+                if "K" in match.group().upper():
+                    last_month_sold = int(last_month_sold[:-1])
+                    last_month_sold *= 1000
+                elif "M" in match.group().upper():
+                    last_month_sold = int(last_month_sold[:-1])
+                    last_month_sold *= 1000000
+                else:
+                    last_month_sold = int(last_month_sold)
+
+            # CREATED_DATE
+            created_date = datetime.datetime.now()
+
             print(f"CATEGORY_ASIN: {category_asin}")
             print(f"CATEGORY_NAME: {category_name}")
+            print(f"PRODUCT_ASIN: {product_asin}")
+            print(f"TITLE: {title}")
+            print(f"REVIEW_NUM: {review_num}")
+            print(f"RATING: {rating}")
+            print(f"DESCRIPTION: {description}")
+            print(f"COLOR: {color}")
+            print(f"PRICE: {price}")
+            print(f"STATUS: {status}")
+            print(f"LAST_MONTH_SOLD: {last_month_sold}")
+            print(f"CREATED_DATE: {created_date}")
 
-            # # AVAILIBILITY
-            # avai = self.soup_try_to_find(
-            #     "span", {"class": "a-size-medium a-color-success"}
-            # )
-
-            # # PRICE
-            # price = float(
-            #     re.findall(
-            #         r"\d+\.\d+|\d+",
-            #         self.soup_try_to_find("span", {"class": "aok-offscreen"}),
-            #     )[0]
-            # )
-
-            # # DESCRIPTION
-            # ul_element = self.soup.find(
-            #     "ul", class_="a-unordered-list a-vertical a-spacing-mini"
-            # )
-
-            # # Find all <li> elements within the <ul>
-            # li_elements = ul_element.find_all("li", class_="a-spacing-mini")
-
-            # # Extract and print the descriptions from each <li> element
-            # descriptions = [li.span.text.strip() for li in li_elements]
-
-            # # RATING
-            # rating = self.soup.find(
-            #     "span",
-            #     {"data-hook": "rating-out-of-text"},
-            # ).text.strip()
-            # if rating:
-            #     rating = float(re.search(r"\d+(\.\d+)?", rating).group())
-
-
-def extract_amazon_product_data_from_url(url: str, driver: WebDriver, zipcode: int):
-    wait = WebDriverWait(driver, 10)
-
-    driver.get(url)
-
-    # Choose location according to zipcode
-    wait.until(
-        EC.element_to_be_clickable((By.ID, "nav-global-location-popover-link"))
-    ).click()
-    wait.until(
-        EC.element_to_be_clickable(
-            (By.CSS_SELECTOR, "[data-action='GLUXPostalInputAction']")
-        )
-    ).send_keys("92104")
-    wait.until(
-        EC.element_to_be_clickable(
-            (By.CSS_SELECTOR, "[aria-labelledby='GLUXZipUpdate-announce']")
-        )
-    ).click()
-    wait.until(
-        EC.element_to_be_clickable(
-            (By.CSS_SELECTOR, ".a-popover-footer #GLUXConfirmClose")
-        )
-    ).click()
-
-    # Extract the product title (outside the loop as it's the same for all colors)
-    product_title = (
-        WebDriverWait(driver, 10)
-        .until(EC.presence_of_element_located((By.ID, "productTitle")))
-        .text
-    )
-
-    driver.refresh()
-
-    # INITIALIZE_BS4
-    soup = BeautifulSoup(driver.page_source, "html.parser")
-
-    # TITLE
-    title = soup.find("span", {"id": "productTitle"}).text.strip()
-
-    # AVAILIBILITY
-    avai = soup.find("span", {"class": "a-size-medium a-color-success"}).text.strip()
-
-    # PRICE
-    price_whole = soup.find("span", {"class": "a-price-whole"}).text.strip()
-    if price_whole:
-        price_whole = int(re.match(r"\d+", price_whole).group())
-
-    price_fraction = soup.find("span", {"class": "a-price-fraction"}).text.strip()
-
-    price_unit = soup.find("span", {"class": "a-price-symbol"}).text.strip()
-
-    # DESCRIPTION
-    ul_element = soup.find("ul", class_="a-unordered-list a-vertical a-spacing-mini")
-
-    # Find all <li> elements within the <ul>
-    li_elements = ul_element.find_all("li", class_="a-spacing-mini")
-
-    # Extract and print the descriptions from each <li> element
-    descriptions = [li.span.text.strip() for li in li_elements]
-
-    # RATING
-    rating = soup.find(
-        "span",
-        {"data-hook": "rating-out-of-text"},
-    ).text.strip()
-    if rating:
-        rating = float(re.search(r"\d+(\.\d+)?", rating).group())
-
-    # REVIEWS
-    reviews = soup.find("span", {"id": "acrCustomerReviewText"}).text.strip()
-    if reviews:
-        reviews = int(re.match(r"\d+", reviews).group())
-
-    # PRINT INFO
-    print(f"Title: {title}")
-    print(f"Availability: {avai}")
-    print(f"Price: {price_whole}.{price_fraction}")
-    print(f"Price unit: {price_unit}")
-    print(f"Descriptions: {descriptions}")
-    print(f"Rating: {rating}")
-    print(f"Reviews: {reviews}")
+            print("=" * 20, end="\n\n")
 
 
 def main():
     URLs = [
-        "https://www.amazon.com/COSIBONO-Pumpkin-Decorative-Harvest-Farmhouse/dp/B0D5YM77ML/ref=zg_bsnr_c_home-garden_d_sccl_4/141-3078590-5074935?pd_rd_w=D5QLJ&content-id=amzn1.sym.7379aab7-0dd8-4729-b0b5-2074f1cb413d&pf_rd_p=7379aab7-0dd8-4729-b0b5-2074f1cb413d&pf_rd_r=SFW6WCTP49M4KPEMC58E&pd_rd_wg=TrGpP&pd_rd_r=f059d5b7-30fb-485a-8a07-65c36aaaa1c3&pd_rd_i=B0D5YM77ML&th=1",
-        # "https://www.amazon.com/Amelity-Headrest-Storage-Leather-Black-2/dp/B0CX18TFQD/ref=zg_bsnr_c_automotive_d_sccl_1/141-3078590-5074935?pd_rd_w=TKw5q&content-id=amzn1.sym.7379aab7-0dd8-4729-b0b5-2074f1cb413d&pf_rd_p=7379aab7-0dd8-4729-b0b5-2074f1cb413d&pf_rd_r=FS98Z2RHTK16P5S8J7QC&pd_rd_wg=UAZ3a&pd_rd_r=3c539dc2-8a71-4ce0-9fa2-54e61e4b3277&pd_rd_i=B0CX18TFQD&th=1",
-        # "https://www.amazon.com/FADAKWALT-Inflator-Portable-Compressor-Motorcycles/dp/B0D1MZ5ZTK/ref=zg_bsnr_c_automotive_d_sccl_2/141-3078590-5074935?pd_rd_w=TKw5q&content-id=amzn1.sym.7379aab7-0dd8-4729-b0b5-2074f1cb413d&pf_rd_p=7379aab7-0dd8-4729-b0b5-2074f1cb413d&pf_rd_r=FS98Z2RHTK16P5S8J7QC&pd_rd_wg=UAZ3a&pd_rd_r=3c539dc2-8a71-4ce0-9fa2-54e61e4b3277&pd_rd_i=B0D1MZ5ZTK&th=1",
+        "https://www.amazon.com/Amelity-Headrest-Storage-Leather-Black-2/dp/B0CX18TFQD/ref=zg_bsnr_c_automotive_d_sccl_1/141-3078590-5074935?pd_rd_w=MABN3&content-id=amzn1.sym.7379aab7-0dd8-4729-b0b5-2074f1cb413d&pf_rd_p=7379aab7-0dd8-4729-b0b5-2074f1cb413d&pf_rd_r=0Y582SYJA52XPQ46D08Z&pd_rd_wg=WBZ2b&pd_rd_r=d16f64de-fd58-48a9-b10c-27becea887e2&pd_rd_i=B0CX18TFQD&th=1",
+        "https://www.amazon.com/BAIBOSHI-Pack-Handle-Assist-Elderly/dp/B0D9GYK52D/ref=zg_bsnr_c_automotive_d_sccl_2/141-3078590-5074935?pd_rd_w=MABN3&content-id=amzn1.sym.7379aab7-0dd8-4729-b0b5-2074f1cb413d&pf_rd_p=7379aab7-0dd8-4729-b0b5-2074f1cb413d&pf_rd_r=0Y582SYJA52XPQ46D08Z&pd_rd_wg=WBZ2b&pd_rd_r=d16f64de-fd58-48a9-b10c-27becea887e2&pd_rd_i=B0D9GYK52D&th=1",
+        "https://www.amazon.com/ZGZUXO-Inflator-Compressor-Rechargeable-Motorcycle/dp/B0D1C77YTL/ref=zg_bsnr_c_automotive_d_sccl_3/141-3078590-5074935?pd_rd_w=MABN3&content-id=amzn1.sym.7379aab7-0dd8-4729-b0b5-2074f1cb413d&pf_rd_p=7379aab7-0dd8-4729-b0b5-2074f1cb413d&pf_rd_r=0Y582SYJA52XPQ46D08Z&pd_rd_wg=WBZ2b&pd_rd_r=d16f64de-fd58-48a9-b10c-27becea887e2&pd_rd_i=B0D1C77YTL&th=1",
     ]
-
-    # OPTIONS = Options()
-    # # OPTIONS.add_argument("--headless=new")
-
-    # DRIVER = webdriver.Chrome(options=OPTIONS)
-
-    # ZIPCODE = 92104
-
-    # for URL in URLs:
-    #     extract_amazon_product_data_from_url(URL, DRIVER, ZIPCODE)
 
     extractor = Extactor()
     extractor.extract_amazon_product_data_from_url(URLs)
